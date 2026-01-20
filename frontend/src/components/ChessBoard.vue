8953c4016cef42bc4769ad91937736783b5595a2
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-20 19:12:03 +0200

    Add pawns

--- frontend/src/components/ChessBoard.vue
+++ frontend/src/components/ChessBoard.vue
@@ -9,6 +9,27 @@ const isDark = (n: number) => {
    <div class="chess-board-container">
      <div v-for="n in 64" :key="n" class="square" :class="{ dark: isDark(n) }"></div>
    </div>
+
+   <div class="black-pieces">
+    <span class="pawns pawn1">&#9823;</span>
+    <span class="pawns pawn2">&#9823;</span>
+    <span class="pawns pawn3">&#9823;</span>
+    <span class="pawns pawn4">&#9823;</span>
+    <span class="pawns pawn5">&#9823;</span>
+    <span class="pawns pawn6">&#9823;</span>
+    <span class="pawns pawn7">&#9823;</span>
+    <span class="pawns pawn8">&#9823;</span>
+   </div>
+   <div class="white-pieces">
+    <span class="w-pawns w-pawn1">&#9823;</span>
+    <span class="w-pawns w-pawn2">&#9823;</span>
+    <span class="w-pawns w-pawn3">&#9823;</span>
+    <span class="w-pawns w-pawn4">&#9823;</span>
+    <span class="w-pawns w-pawn5">&#9823;</span>
+    <span class="w-pawns w-pawn6">&#9823;</span>
+    <span class="w-pawns w-pawn7">&#9823;</span>
+    <span class="w-pawns w-pawn8">&#9823;</span>
+   </div>
 </template>
 
 <style scoped>
@@ -33,5 +54,33 @@ const isDark = (n: number) => {
 
 .square.dark {
   background: #b58863;
+}
+.pawn1 { left: 29.3rem }
+.pawn2 { left: 35rem; }
+.pawn3 { left: 40.7rem; }
+.pawn4 { left: 46.2rem; }
+.pawn5 { left: 51.9rem; }
+.pawn6 { left: 57.6rem; }
+.pawn7 { left: 63.3rem; }
+.pawn8 { left: 69rem; }
+
+.pawns {
+  position: absolute;
+  font-size: 3rem;
+  top: 9.2rem;
+}
+.w-pawn1 { left: 29.3rem }
+.w-pawn2 { left: 35rem; }
+.w-pawn3 { left: 40.7rem; }
+.w-pawn4 { left: 46.2rem; }
+.w-pawn5 { left: 51.9rem; }
+.w-pawn6 { left: 57.6rem; }
+.w-pawn7 { left: 63.3rem; }
+.w-pawn8 { left: 69rem; }
+.w-pawns {
+  position: absolute;
+  font-size: 3rem;
+  top: 37.2rem;
+  color: white;
 }
 </style>
